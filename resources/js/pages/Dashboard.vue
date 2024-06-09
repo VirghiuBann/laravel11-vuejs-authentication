98070a0cbee3039e3e4dbec99ccb5a1f6baebb58
virghiu <virghiu@gmail.com>
2024-06-08 23:31:45 -0400

    render user name in dashboard

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { useAuthStore } from "@/stores";
+
+const store = useAuthStore();
+const user = store.getUser;
+</script>
 <template>
     <div class="navbar bg-base-100">
         <div class="flex-1">
@@ -25,6 +31,7 @@
                                 d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                             />
                         </svg>
+
                         <span class="badge badge-sm indicator-item">8</span>
                     </div>
                 </div>
@@ -61,6 +68,9 @@
                     class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
                 >
                     <li>
+                        <p class="justify-center capitalize">{{ user.name }}</p>
+                    </li>
+                    <li>
                         <a class="justify-between">
                             Profile
                             <span class="badge">New</span>
@@ -73,7 +83,5 @@
         </div>
     </div>
 </template>
-
-<script setup></script>
 
 <style lang="scss" scoped></style>
