ae9f8017f8840e48b6daf3018dc8b7e30530e84d
virghiu <virghiu@gmail.com>
2024-06-10 18:41:21 -0400

    group middleware

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,15 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::middleware(['guest'])->group(function () {
+    Route::post('/register', [RegisterController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
+
 Route::get('/{any}', function () {
     return view('main');
 })->where('any', '.*');
-
-Route::post('/register', [RegisterController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login'])->middleware('guest');
-
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
